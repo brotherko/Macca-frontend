a28b760a7fd489e3e70e69aef1f0caaabd531dde
Leo So <leosohc@gmail.com>
2019-05-07 14:31:40 +0800

    Get the option from server

--- src/app/screens/Select_Lang.js
+++ src/app/screens/Select_Lang.js
@@ -2,12 +2,43 @@ import React, {Component} from "react";
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 
-export default class Select_Lang extends React.Component{
+import Select from 'react-select'
+
+const option = []
+export class Select_Lang extends React.Component{
+	componentDidUpdate(){
+		const {list_languages} = this.props.list_languagesQuery
+		for(var i = 0; i < list_languages.length; i++){
+			var tmp = {
+				value: list_languages[i].id,
+				label: list_languages[i].name
+			}
+			option.push(tmp)
+		}
+	}
 	render(){
+		console.log(this.props)
 		return(
-			<h1>Select Language</h1>
+			<div>
+				<h1>Select Language</h1>
+				
+			</div>
 		)
 	}
 }
+
+const list_languages = gql`
+	query list_languages{
+		list_languages {
+			id
+			name
+		}
+	}
+`
+
+export default compose(
+	graphql(list_languages, {name: "list_languagesQuery"})
+)(Select_Lang)
+
 
 
