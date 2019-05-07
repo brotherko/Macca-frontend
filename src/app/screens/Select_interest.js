fec097b12965644f1c5d607250e9048e5ee20947
Leo So <leosohc@gmail.com>
2019-05-07 13:03:14 +0800

    Control the scale button

--- src/app/screens/Select_interest.js
+++ src/app/screens/Select_interest.js
@@ -3,7 +3,26 @@ import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 
 export class Select_Interest extends React.Component {
-
+	state = {
+		interest: []
+	}
+	add_scale = (id) => {
+		console.log(id)
+		var tmp = this.state.interest
+		if(this.state.interest[id]){
+			if(tmp[id] < 5){
+				var newScale = this.state.interest[id]
+				newScale++
+				tmp[id] = newScale
+			}
+		}else{
+			tmp[id] = 1
+		}
+		this.setState({
+			interest: tmp
+		})
+		console.log(this.state.interest)
+	}
 	render() {
 		console.log(this.props);
 		return(
@@ -14,7 +33,7 @@ export class Select_Interest extends React.Component {
 						?<div>
 							{this.props.list_interestQuery.list_interests.map((interest, i) => {
 								return(
-									<button key={i}>{interest.name}</button>
+									<button key={i} onClick={this.add_scale.bind(this, interest.id)}>{interest.name}</button>
 								)
 							})
 						}
@@ -22,6 +41,8 @@ export class Select_Interest extends React.Component {
 						:null
 					}
 				</div>
+				<br></br>
+				<button>Continue</button>
 			</div>
 		)
 	}
@@ -36,9 +57,9 @@ const list_interest = gql`
 	}
 `
 
-// export default compose(
-//   graphql(list_interest, {name: 'list_interestQuery'})
-//   // graphql(insert_chats_message, {name: 'insert_chat_messageMutation'})
-// )(Select_Interest);
+export default compose(
+  graphql(list_interest, {name: 'list_interestQuery'})
+  // graphql(insert_chats_message, {name: 'insert_chat_messageMutation'})
+)(Select_Interest);
 
-export default graphql(list_interest, {name: 'list_interestQuery'})(Select_Interest)
+// export default graphql(list_interest, {name: 'list_interestQuery'})(Select_Interest)
