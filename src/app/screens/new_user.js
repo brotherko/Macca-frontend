b24342da75e31c911a90c0b9dc32f15c97566f46
Leo So <leosohc@gmail.com>
2019-05-07 20:07:23 +0800

    Add user_id when add new user

--- src/app/screens/new_user.js
+++ src/app/screens/new_user.js
@@ -2,14 +2,34 @@ import React, {Component} from "react";
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 
-export default class New_user extends React.Component{
+export class New_user extends React.Component{
 	state = {
-		name: null
+		name: null,
+		user_id: null
 	}
 	
 	handleSubmit = (e) => {
+		const {cookies} = this.props
 		console.log(this.state.name)
 		e.preventDefault();
+		//insert the name
+		this.props.insert_usersMutation({
+			variables: {
+				name: this.state.name
+			}
+		}).then(res =>{
+			var tmp = res.data.insert_users.returning[0].id
+			this.setState({
+				user_id: tmp
+			})
+			document.cookie = "user_id=" + this.state.user_id 
+			this.props.history.push("/interest")
+		})
+		//get the user_id (done above)
+		
+		//put in the cookie/place for holding the id
+		// go to the interest
+		
 	}
 
 	handleChange = (e) => {
@@ -19,6 +39,7 @@ export default class New_user extends React.Component{
 	}
 
 	render(){
+		console.log(this.props)
 		return(
 			<div>
 				<h1>New Name</h1>
@@ -31,3 +52,16 @@ export default class New_user extends React.Component{
 		)
 	}
 }
+
+const insert_users = gql`
+	mutation insert_users($name: String!){
+		insert_users(objects: {name: $name}) {
+			returning {
+				id
+				name
+			}
+		}
+	}
+`
+
+export default graphql(insert_users, {name: "insert_usersMutation"})(New_user)
