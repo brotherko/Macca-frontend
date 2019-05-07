78b951f68fb52d6a1452be839ad3d973a3786f62
Leo So <leosohc@gmail.com>
2019-05-07 15:03:27 +0800

    Can insert the user_lang

--- src/app/screens/Select_Lang.js
+++ src/app/screens/Select_Lang.js
@@ -4,8 +4,12 @@ import gql from 'graphql-tag';
 
 import Select from 'react-select'
 
-const option = []
+
 export class Select_Lang extends React.Component{
+	state = {
+		native_lang: null,
+		target_lang: null
+	}
 	componentDidUpdate(){
 		const {list_languages} = this.props.list_languagesQuery
 		for(var i = 0; i < list_languages.length; i++){
@@ -16,12 +20,58 @@ export class Select_Lang extends React.Component{
 			option.push(tmp)
 		}
 	}
+
+	handleNativeChange = (native_lang) => {
+		this.setState({native_lang});
+	}
+
+	handleTargetChange = (target_lang) => {
+		this.setState({target_lang})
+	}
+
+	handleFind = () => {
+		//mutate the native
+		this.props.insert_users_native_langsMutation({
+			variables: {
+				lang_id: this.state.native_lang.value,
+				user_id: 540
+			}
+		})
+		//mutate the target
+		this.props.insert_users_target_langsMutation({
+			variables: {
+				lang_id: this.state.target_lang.value,
+				user_id: 540
+			}
+		})
+		//go to search page
+
+	}
 	render(){
+		const {native_lang, target_lang} = this.state
+		console.log(this.state)
 		console.log(this.props)
+
 		return(
 			<div>
 				<h1>Select Language</h1>
-				
+				<div className="select">
+					<div>
+							<label>Select your native Language</label>
+							<Select
+								value={native_lang}
+								onChange={this.handleNativeChange}
+								options={option}
+							/>
+							<label>Select your target learning Language</label>
+							<Select
+								value={target_lang}
+								onChange={this.handleTargetChange}
+								options={option}
+							/>
+						</div>
+				</div>
+				<button onClick={this.handleFind}>Search for your buddy</button>
 			</div>
 		)
 	}
@@ -35,9 +85,35 @@ const list_languages = gql`
 		}
 	}
 `
+const option = []
+
+const insert_users_native_langs = gql`
+	mutation insert_users_native_langs ($lang_id: Int!, $user_id: Int!){
+		insert_users_native_langs(objects: {lang_id: $lang_id, user_id: $user_id}) {
+			returning {
+				id
+				lang_id
+				user_id
+			}
+		}
+	}
+`
+const insert_users_target_langs = gql`
+	mutation insert_users_target_langs ($lang_id: Int!, $user_id: Int!){
+		insert_users_target_langs(objects: {lang_id: $lang_id, user_id: $user_id}) {
+			returning {
+				id
+				lang_id
+				user_id
+			}
+		}
+	}
+`
 
 export default compose(
-	graphql(list_languages, {name: "list_languagesQuery"})
+	graphql(list_languages, {name: "list_languagesQuery"}),
+	graphql(insert_users_native_langs, {name: "insert_users_native_langsMutation"}),
+	graphql(insert_users_target_langs, {name: "insert_users_target_langsMutation"}),
 )(Select_Lang)
 
 
