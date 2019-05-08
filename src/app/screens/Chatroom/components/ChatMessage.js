df2dafa830afb566ec3371aab70e2fe0fcfaaac4
Leo So <leosohc@gmail.com>
2019-05-09 02:27:31 +0800

    delete console.log

--- src/app/screens/Chatroom/components/ChatMessage.js
+++ src/app/screens/Chatroom/components/ChatMessage.js
@@ -33,7 +33,6 @@ class ChatMessage extends Component {
       ))
     )
     : null
-    console.log(this.props)
     return (
       <div className='ChatMessage'>
         {chats_correction && chats_correction.length > 0
